1d29505e558e517732a9f0dae66886058de96241
Eddie C <eddiec2024@gmail.com>
2021-05-03 19:58:48 -0700

    saving work, saving needs to be all rewritten.

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ console.log("Starting ideatree...");
 
 let interval = 1;
 let blockReward = 10000;
-let recentActivities = 500; // affects the hash rate.
-let activeMembers = 100; // affects the hash rate.
+let recentActivities = 50000; // affects the hash rate.
+let activeMembers = 100000; // affects the hash rate.
 // let myStake = votes/totalVotes;
 let difficulty = recentActivities + 100000 - 0; // higher more difficult
 const blockSize = 3888;
@@ -49,15 +49,8 @@ const walletTree = new WalletTree();
 
 const lifecycle = setInterval(() => {
     if (!walletTree.isReady) {
-        console.log(`Waiting for initialization... ${(new Date().getTime() - startTime)/1000}s elapsed.`);
+        console.log(`Waiting for ready state... ${(new Date().getTime() - startTime)/1000}s elapsed.`);
         return;
-    }
-
-    walletTree.log();
-    if (walletTree.userWallets.length <= 0) {
-        for(let i=0; i<3; i++) {
-            walletTree.createWallet(`test${i}`);
-        }
     }
 
     // traverse through the community tree and reward.
@@ -72,17 +65,26 @@ const lifecycle = setInterval(() => {
     for (let i = 0; i < tryCounts; i++) {
         const guess = Math.floor(Math.random() * difficulty);
 
-        if (systemCode == guess) {
+        if (systemCode == guess && walletTree.systemWallet.balance > 1) {
             console.log(`Cracked the code '${systemCode}' at ${i}th out of ${tryCounts} tries!!\n`);
+            if (blockReward >= walletTree.systemWallet.balance) {
+                blockReward = walletTree.systemWallet.balance;
+            }
+
+            let totalPower = 0;
+            walletTree.userWallets.forEach(w => {
+                totalPower += w.hashPower;
+            });
 
             walletTree.userWallets.forEach(w => {
-                const stake = w.hashPower / recentActivities;
+                const stake = w.hashPower / totalPower;
                 const amount = blockReward * stake;
+
                 walletTree.createTransaction(w.id, amount);
             });
 
-            // console.log(`${tree.wallets.length} active wallets rewarded total ${blockReward} MDC!, System balance: ${systemTotal}`);
-            // tree.log();
+            console.log(`${walletTree.userWallets.length} active wallets rewarded total ${blockReward} MDC!, System balance: ${walletTree.systemWallet.balance} MDC`);
+            walletTree.log();
             break;
         }
     }
@@ -93,18 +95,28 @@ const lifecycle = setInterval(() => {
         console.log(`Interval: ${interval}, ${interval / 60 / 60} hrs\n`);
     }
 
-    if (interval % blockSize == 0) {
-        console.log(`Interval at: ${interval}...`);
-        const previousReward = blockReward;
-        blockReward = blockReward / 2;
-        console.log(`Having block rewards from ${previousReward} to ${blockReward}...\n`);
+    // if (interval % blockSize == 0) {
+    //     console.log(`Interval at: ${interval}...`);
+    //     const previousReward = blockReward;
+    //     blockReward = blockReward / 2;
+    //     console.log(`Having block rewards from ${previousReward} to ${blockReward}...\n`);
 
-        // clearInterval(lifecycle);
-        walletTree.save(interval, interval > 0 ? interval - blockSize : 0, interval + blockSize);
+    //     // clearInterval(lifecycle);
+    //     // walletTree.save(interval, interval > 0 ? interval - blockSize : 0, interval + blockSize);
+    // }
+
+    if (interval % 10 == 0) {
+        walletTree.log();
+
+        console.log(`Full validation and ledger write every 30s... Ledger locked.`);
+        walletTree.isReady = false;
+        walletTree.save();
+        walletTree.isReady = true;
     }
 
     switch (interval) {
-        case 3:
+        case 5:
+            // walletTree.createTransaction('b6606a0c-6834-4e55-aa1d-b5083051f8be', 30000, 'f0de7f7c-d49a-4a97-8962-5b1041a7ee44');
             console.log(`Test scenario on interval: ${interval} ended...\n`);
             break;
         default:
@@ -112,4 +124,4 @@ const lifecycle = setInterval(() => {
     }
 
     interval++;
-}, 1);
+}, 500);
--- models/WalletTree.js
+++ models/WalletTree.js
@@ -9,11 +9,14 @@ export class WalletTree {
         this.isReady = false;
         this.treeFactory = createTree();
 
+        this.blockSize;
         this.genesisBlockId;
-        this.currentBlockId;
+        this.localLatestBlockId;
 
         this.userWallets = [];
         this.transactions = [];
+        this.blocks = {};
+        this.blockIds = [];
 
         this.systemWallet;
         this.tree;
@@ -24,7 +27,7 @@ export class WalletTree {
                     console.log(`Data directory, '${dataDir}' doesn't exist. Need to download, extract.`);
                     throw err;
                 }
-    
+
                 console.log(`Block data directory exists!`);
                 const blockIds = [];
                 files.forEach(f => {
@@ -33,7 +36,7 @@ export class WalletTree {
 
                 if (blockIds.length > 0) {
                     this.genesisBlockId = Math.min(...blockIds);
-                    this.currentBlockId = Math.max(...blockIds);
+                    this.localLatestBlockId = Math.max(...blockIds);
                     this.addAllNodes(dataDir);
                 }
             });
@@ -42,32 +45,53 @@ export class WalletTree {
         }
     }
 
-    addAllNodes(dataDir, nextBlockId = this.genesisBlockId) {
-        console.log(`Loading block id ${nextBlockId}...`);
-        jsonfile.readFile(`${dataDir}/${nextBlockId}`, (err, obj) => {
+    addAllNodes(dataDir, blockId = this.genesisBlockId) {
+        console.log(`Loading block id ${blockId}...`);
+        jsonfile.readFile(`${dataDir}/${blockId}`, (err, obj) => {
             if (err) throw err;
             const nodes = obj.nodes;
-            nextBlockId = obj.next;
+            blockId = obj.next;
+
+            const blockSize = obj.next - obj.id;
+            if (!this.blockSize) {
+                this.blockSize = blockSize;
+            } else {
+                if (this.blockSize != blockSize) {
+                    throw { error: "Inconsistent block size detected. Aborting!" };
+                }
+            }
 
             console.log(`Synchronizing ${nodes.length} nodes...`);
             nodes.forEach(node => {
-                this.tree = !this.tree ? this.treeFactory.insert(node.id, node) : this.tree.insert(node);
+                this.tree = !this.tree ? this.treeFactory.insert(node.id, node) : this.tree.insert(node.id, node);
                 if (node.type === 'w') {
-                    if (node.alias === 'root') {
+                    if (node.alias === 'root' && !this.systemWallet || this.systemWallet.balance > node.balance) {
                         this.systemWallet = node;
-                    } else {
+                    } else if (node.alias !== 'root') {
                         this.userWallets.push(node);
                     }
-                } else if (obj.type === 'tx') {
+                } else if (node.type === 'tx') {
                     this.transactions.push(node);
+                } else {
+                    throw { error: "Unknown type in the blocks. Abort!" };
                 }
             });
 
-            if (nextBlockId < this.currentBlockId) {
-                this.addAllNodes(dataDir, nextBlockId);
+            this.blocks[blockId] = nodes;
+            this.blockIds.push(blockId);
+
+            if (blockId <= this.localLatestBlockId) {
+                this.addAllNodes(dataDir, blockId);
             } else {
                 this.isReady = true;
                 console.log(`Next block doesn't exist. Completed adding all nodes!\n`);
+
+                if (this.userWallets.length <= 0) {
+                    console.log(`If there is 0 user wallets, create few test wallets...`);
+                    for (let i = 0; i < 3; i++) {
+                        this.createWallet(`test${i}`);
+                    }
+                }
             }
         })
     }
@@ -93,26 +117,34 @@ export class WalletTree {
             return;
         }
 
+        const fromWallet = from.value;
+
         const to = this.tree.find(toAddress);
         if (!to) {
             console.log(`Invalid address passed in, address '${toAddress}' not found`);
             return;
         }
 
-        if (from.value.balance < amount) {
-            console.log(`Source address '${from.value.id}' doesn't have enough balance.`);
+        const toWallet = to.value;
+
+        if (fromWallet.balance < amount) {
+            console.log(`Source address '${fromWallet.id}' doesn't have enough balance.`);
             return;
         }
 
-        const transaction = new Transaction(from.value.id, to.value.id, amount);
-        from.value.balance -= amount;
-        to.value.balance += amount;
+        const transaction = new Transaction(fromWallet.id, toWallet.id, amount);
+        fromWallet.balance -= amount;
+        toWallet.balance += amount;
+
         this.tree = this.tree.insert(transaction.id, transaction);
         this.transactions.push(transaction);
 
+        // this.tree = this.tree.insert(fromWallet.id, fromWallet);
+        // this.tree = this.tree.insert(toWallet.id, toWallet);
+
         console.log(`New balance confirmed by 100 nodes!, queued for thorough fraud detection...`);
-        console.log(`Balance (${from.value.id}): ${from.value.balance}`);
-        console.log(`Balance (${to.value.id}): ${to.value.balance}\n`);
+        console.log(`Balance (${fromWallet.id}): ${fromWallet.balance}`);
+        console.log(`Balance (${toWallet.id}): ${toWallet.balance}\n`);
     }
 
     log() {
@@ -134,19 +166,57 @@ export class WalletTree {
         console.log(`--------------------------------------------\n`);
     }
 
-    save(checkpoint, previous, next) {
-        const block = {
-            id: checkpoint,
-            previous: previous,
-            next: next,
+    save() {
+        this.tree.forEach((id, node) => {
+            if (this.blocks[newBlock.id].indexOf(node) < 0) {
+                this.blocks[newBlock.id].push(node);
+            }
+
+            if (this.blocks[newBlock.id].length >= this.blockSize) {
+                // saving full block
+                newBlock.nodes = this.blocks[newBlock.id];
+                write.sync(`./blocks/${newBlock.id}`, JSON.stringify(newBlock));
+
+                // create next block, to be filled-in next loops
+                newBlock.previous = newBlock.id;
+                newBlock.id = newBlock.next;
+                newBlock.next += newBlock.id + this.blockSize;
+                this.blocks[newBlock.id] = [];
+            }
+        });
+
+        const newBlockId = this.localLatestBlockId + this.blockSize;
+        const newBlock = {
+            id: newBlockId,
+            previous: this.localLatestBlockId,
+            next: newBlockId + this.blockSize,
             nodes: []
         }
 
-        const nodes = [];
+        if (!this.blocks[newBlock.id]) {
+            this.blocks[newBlock.id] = [];
+        }
+
         this.tree.forEach((id, node) => {
-            block.nodes.push(node);
+            if (this.blocks[newBlock.id].indexOf(node) < 0) {
+                this.blocks[newBlock.id].push(node);
+            }
+
+            if (this.blocks[newBlock.id].length >= this.blockSize) {
+                // saving full block
+                newBlock.nodes = this.blocks[newBlock.id];
+                write.sync(`./blocks/${newBlock.id}`, JSON.stringify(newBlock));
+
+                // create next block, to be filled-in next loops
+                newBlock.previous = newBlock.id;
+                newBlock.id = newBlock.next;
+                newBlock.next += newBlock.id + this.blockSize;
+                this.blocks[newBlock.id] = [];
+            }
         });
 
-        write.sync(`./blocks/${block.id}`, JSON.stringify(block));
+        write.sync(`./blocks/${newBlock.id}`, JSON.stringify(newBlock));
+        this.localLatestBlockId = newBlockId;
+        console.log(`Write complete...`);
     }
 }
